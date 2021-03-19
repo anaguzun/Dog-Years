12ba8c4ede642a0ff5b861f3f7d4a6a18054ddb6
anaguzun <ana.busuioc@yahoo.com>
2021-03-19 15:45:07 +0100

    dog years exercise

---
+++ dogyears.js
@@ -0,0 +1,15 @@
+// constatnt varianble for my age.
+const myAge = 32 ;
+// early years for a dog.
+let earlyYears = 2 ;
+earlyYears *= 10.5 ;
+// variable laterYears to calculate the number of dor years accounted for your later years.
+let laterYears = myAge - 2 ;
+laterYears *= 4 ;
+// calculatin my age in dog years.
+let myAgeInDogYears = earlyYears + laterYears ;
+//const variable for my name. string method.tolowwerCase to return all letters lowercase.
+const myName = 'Ana'
+myName.toLowerCase();
+// display the results.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years olg in dog years.`);
